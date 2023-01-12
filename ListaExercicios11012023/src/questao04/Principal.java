3045f6c593f28b09af2710acbe1718dec563ee81
Eric <ericsales0@gmail.com>
2023-01-11 21:13:58 -0300

    Questão 04

---
+++ ListaExercicios11012023/src/questao04/Principal.java
@@ -0,0 +1,16 @@
+package questao04;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Produto produto01 = new Produto();
+		produto01.setDescricao("Detergente");
+		produto01.setPreco(2.31);
+		
+		Produto produto02 = new Produto();
+		produto01.setDescricao("Leite");
+		produto01.setPreco(4.15);
+		
+	}
+
+}
